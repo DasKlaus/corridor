21e8f4752c4430c655e47ab47c0b79bd6e5ffb8d
Klaus Hilgenfelder <klaus.hilgenfelder@gmx.de>
2016-05-21 20:10:24 +0200

    Draw enums, WIP

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 columnHeight = $(window).height()-100;
 columnWidth = 150;
-cellDrawSpeed = 1; // ms for each cell placement
+cellDrawSpeed = 100; // ms for each cell placement
 // TODO: make these configurable and responding to resize
 // TODO: nest variables and functions into an object, so there's no accidental overloading when using this as a library
 
@@ -33,11 +33,30 @@ function drawCell(data, svg) {
 		if (dataStructure[i].type=="id") row = data[i];
 	}
 	d3.select("svg[index='"+index+"'] .chart").append("circle")
-		.attr("cy", function(d) {return Math.round(scale(data[index]));})
+		.attr("cy", function(d) {
+			switch(dataStructure[index].type) {
+				case "enum":
+					var coords = scale(data[index]);
+					var center = coords[0];
+					var height = coords[1];
+					// place cell randomly in a Gaussian distribution in the area
+					return Math.round(makeGauss(center, height/4-2*radius, function(value){
+						// prevent value from deviating outside of the placement area
+						if (value>2 || value<-2) {
+							value = makeGauss(0, 1); // TODO: nope!
+						} 
+						return value;}));
+					break;
+				case "int":
+				case "float":
+				default:
+					return Math.round(scale(data[index]));
+			}
+		})
 		.attr("cx", function(d) {
 			// approximate distribution of cells over x axis
 			// TODO: refine placement on requestAnimationFrame (collision detection)
-			var y = Math.round(scale(data[index]));
+			var y = this.getAttribute("cy");
 			// get all cells that are already placed around the same y value
 			var same = svg.selectAll(".cell").filter(function(d) {
 					if (!d3.select(this).attr("value")) return false;
@@ -47,8 +66,9 @@ function drawCell(data, svg) {
 				if (radius>0) {
 					radius--;
 					svg.attr("radius", radius);
+					// all cells already drawn need to be redrawn
 					svg.selectAll(".cell").each(function() {
-						// TODO: transitions!
+						// TODO: transitions?
 						var cell = d3.select(this);
 						cell.attr("r", function(d){return (radius>0)?radius:1;}).attr("cx", function() {
 							// calculate new x position
@@ -121,31 +141,97 @@ function drawColumn(select) {
 function drawAxis(structure, svg) {
 	// remove if already drawn
 	svg.select(".axis").remove();
-	// generate axis
-	var axis = d3.svg.axis()
-	    .scale(structure.scale) // TODO: if enum, this will be different
-	    .orient("left")
-	    .ticks(20); // TODO: if enum, this will be different
-	// TODO: draw boundaries if applicable
-	svg.append("g").attr("transform", "translate(40,0)").attr("class","axis").call(axis); // TODO: what if labels are wider than 40px? big numbers? strings?
+	switch(structure.type) {
+		case "enum": break; // TODO
+		case "int":
+		case "float":
+		default: var axis = d3.svg.axis()
+			    .scale(structure.scale)
+			    .orient("left")
+			    .ticks(20);
+			// TODO: draw boundaries if applicable
+			svg.append("g").attr("transform", "translate(40,0)").attr("class","axis").call(axis); // TODO: what if labels are wider than 40px? big numbers? strings?
+	}
 }
 
 function checkScale(structure, data, svg) {
 	if (!structure.scale) { // if no scale is set, create a new one and draw its axis
 		switch(structure.type) {
-			case "enum": break; // TODO
+			case "enum": structure.total = 1; // absolute number of datasets drawn
+				structure.percent = 100.0; // added percentages, always 100 if not an array of enums
+				structure.values = new Array({value: data, total: 1, percent: 100, offset: 0}); // only one value yet known
+				structure.boundaries = new Array(); // only one value, so no boundaries
+				// make helper scale for pixel calculation from percent values
+				structure.topixel = d3.scale.linear().domain([0,structure.percent]).range([columnHeight,0]);
+				// scale is a custom function that returns the center of the block for this specific string and its height in pixels for cell placement
+				structure.scale = function(value) {
+					// search for value in structure.values
+					for (var i=0; i<structure.values.length; i++) {
+						if (structure.values[i].value == value) {
+							// get offset and height
+							var offset = structure.topixel(structure.values[i].offset); // lowest point
+							var height = structure.topixel(structure.values[i].percent);
+							// return middle of the placeable area and height
+							return new Array(offset-height/2, height);
+						}
+					}
+				};
+				break;
 			case "int":
 			case "float":
 			default: structure.min = data;
 				structure.max = data; 
 				structure.scale = d3.scale.linear().domain([data-1, data+1]).range([columnHeight,0]);
-				drawAxis(structure, svg);
 		}
+		drawAxis(structure, svg);
 		return;
 	}
 	// check if scale is still correct
 	switch(structure.type) {
-		case "enum": break; // TODO
+		case "enum": 
+			// add cell to total
+			structure.total++;
+			// look for value in values
+			var valueData;
+			for (var i=0; i<structure.values.length; i++) {
+				if (structure.values[i].value == data) 
+					valueData = structure.values[i];
+			}
+			if (valueData) { // value already exists
+				// add counters
+				valueData.total++;
+				// check percentages
+				if (Math.abs(valueData.percent - valueData.total/structure.total*100)<1)
+					return; // no change in scale
+			} else {
+				// add data for new value
+				valueData = {value: data, total: 1, percent: 0, offset: 0}; // percentages and offsets will be recalculated, anyway
+				structure.values.push(valueData);
+			}
+			// recalculate percentages
+			for (var i=0; i<structure.values.length; i++) {
+				structure.values[i].percent = structure.values[i].total/structure.total*100;
+			}
+			// resort by percentages (largest first)
+			structure.values.sort(function(a, b) {
+				var x = a.percent; 
+				var y = b.percent;
+				return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+			});
+			// recalculate offsets and boundaries
+			structure.boundaries = new Array();
+			var offset = 0;
+			for (var i=0; i<structure.values.length; i++) {
+				structure.values[i].offset = offset;
+				if (offset>0) {
+					// add boundary
+					structure.boundaries.push(Math.round(structure.topixel(offset)));
+				}
+				offset = structure.values[i].percent;
+			}
+			drawAxis(structure, svg); // redraw axis
+			// TODO: all cells drawn up to this point need to be repositioned
+			break;
 		case "int":
 		case "float":
 		default: if (data>structure.min && data<structure.max) return; // no change in scale
@@ -167,6 +253,7 @@ function checkScale(structure, data, svg) {
 	}	
 }
 
+// TODO: enum scale should group data below a certain height to "other"
 // TODO: dynamic number of datasets
 // TODO: optional adding group boundaries to floats and ints, or gradients, or colors
 // TODO: dynamic output of grouped data for chart drawing (probably) (later)
